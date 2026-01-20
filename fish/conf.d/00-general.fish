8879b8f28b66ab25e01dd8a5acfbae59a319e5cd
fufu <fuhongrui15@gmail.com>
2026-01-20 18:39:22 +0800

    fish

--- fish/conf.d/00-general.fish
+++ fish/conf.d/00-general.fish
@@ -3,9 +3,22 @@ set -gx EDITOR nvim
 # Set Proxy for GO Dependency
 set -Ux GOPROXY https://goproxy.cn,direct
 
-set -gx http_proxy http://127.0.0.1:12334
-set -gx https_proxy http://127.0.0.1:12334
-set -gx all_proxy socks5://127.0.0.1:12334
+function proxy_on
+    set -gx http_proxy http://127.0.0.1:12334
+    set -gx https_proxy http://127.0.0.1:12334
+    set -gx all_proxy socks5://127.0.0.1:12334
+    # Ignore no_proxy address
+    set -gx no_proxy localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12
+    echo "Proxy enabled."
+end
+
+function proxy_off
+    set -e http_proxy
+    set -e https_proxy
+    set -e all_proxy
+    set -e no_proxy
+    echo "Proxy disabled."
+end
 
 if test -r /etc/os-release
     set -g OS linux
