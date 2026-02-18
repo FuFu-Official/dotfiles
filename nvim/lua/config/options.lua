53661e80ff529be468afa28f68ca13fdbbe7a7a4
fufu <fuhongrui15@gmail.com>
2026-02-18 23:23:13 +0800

    nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.jumpoptions = "stack"
+-- vim.opt.jumpoptions = "stack"
 
 vim.opt.scrolloff = 999
