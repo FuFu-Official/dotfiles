072aa583c92a495398d6de252dbc087b08704285
fufu <fuhongrui15@gmail.com>
2026-03-17 22:00:20 +0800

    scripts

--- .scripts/skip_worktree_matugen.fish
+++ .scripts/skip_worktree_matugen.fish
@@ -2,12 +2,12 @@
 
 set path_prefix $HOME/Dev/dotfiles/
 set matugen_color_file_paths btop/themes/fufu-matugen.theme cava/themes/fufu-matugen gtk-3.0/colors.css \
-    gtk-4.0/colors.css hypr/colors.conf kitty/themes/fufu-matugen.conf mako/mako-colors nvim/matugen.lua \
+    gtk-4.0/colors.css hypr/colors.conf kitty/themes/fufu-matugen.conf mako/mako-colors nvim/lua/matugen/matugen.lua \
     opencode/themes/fufu-matugen.json qt5ct/colors/fufu-matugen.conf qt6ct/colors/fufu-matugen.conf \
     kitty/current-theme.conf rmpc/themes/fufu-matugen.ron yazi/theme.toml wlogout/colors.css \
-    wlogout/.scripts/recolor-icons.sh wlogout/icons/*.png zathura/zathurarc obs-studio/themes/matugen.obt \
+    wlogout/.scripts/recolor-icons.sh wlogout/icons/*.png obs-studio/themes/matugen.obt \
     AdwSteamGtk/custom.css fish/conf.d/50-fish-prompt.fish zen/chrome/colors.css waybar/colors.css \
-    walker/colors.css snappy-switcher/themes/matugen.ini
+    walker/colors.css snappy-switcher/themes/matugen.ini zathura/zathura-colors
 
 for file in $matugen_color_file_paths
     set full_path "$path_prefix$file"
