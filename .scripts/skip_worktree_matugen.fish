2ef02d51bb72d032fc4813c3b4f71f4aa9bd73fe
fufu <fuhongrui15@gmail.com>
2026-05-30 15:49:40 +0800

    scripts: update matugen skip-worktree paths

--- .scripts/skip_worktree_matugen.fish
+++ .scripts/skip_worktree_matugen.fish
@@ -4,10 +4,10 @@ set path_prefix $HOME/Dev/dotfiles/
 set matugen_color_file_paths btop/themes/fufu-matugen.theme cava/themes/fufu-matugen gtk-3.0/colors.css \
     gtk-4.0/colors.css hypr/colors.conf kitty/themes/fufu-matugen.conf mako/mako-colors nvim/lua/matugen/matugen.lua \
     opencode/themes/fufu-matugen.json qt5ct/colors/fufu-matugen.conf qt6ct/colors/fufu-matugen.conf \
-    kitty/current-theme.conf rmpc/themes/fufu-matugen.ron yazi/theme.toml wlogout/colors.css \
+    kitty/current-theme.conf yazi/theme.toml wlogout/colors.css \
     wlogout/.scripts/recolor-icons.sh wlogout/icons/*.png obs-studio/themes/matugen.obt \
     AdwSteamGtk/custom.css fish/conf.d/50-fish-prompt.fish zen/chrome/colors.css waybar/colors.css \
-    walker/colors.css snappy-switcher/themes/matugen.ini zathura/zathura-colors
+    walker/colors.css snappy-switcher/themes/matugen.ini zathura/zathura-colors niri/colors.kdl
 
 for file in $matugen_color_file_paths
     set full_path "$path_prefix$file"
